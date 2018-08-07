5ac2f01d7ce0b2e910b806dc711b2e99fb4ae620
mimigood <mbgss07@gmail.com>
2018-08-07 20:24:11 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,12 @@
 
 def fizzbuzz(number)
   if number % 3 == 0 
-    puts "Fizz"
+    return "Fizz"
+    elsif number % 5 == 0 
+    return "Buzz"
+    elsif number % 5 && 3 
+    return "fizzbuzz"
   end
 end
+
+fizzbuzz("5")
